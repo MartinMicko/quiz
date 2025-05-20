47e8e6a561c89a3cf035b21c2b0fc185dd0e1d39
Sp0oqY <kastierm45@gmail.com>
2025-05-20 23:51:36 +0200

    začal som s podstránkami login a signup ale zatiaľ nie sú prepojené, skúsiť nainštalovať express, ak nie je pretože to mi poradil chat xd, inak by to malo byť dobre aj v quizRoutes.js, glgl

--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -4,4 +4,14 @@ const quizController = require('../controllers/quizController');
 
 router.get('/', quizController.showQuiz);
 
+// Signup page
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+// Home page
+router.get('/', (req, res) => {
+  res.render('home');
+});
+
 module.exports = router;
